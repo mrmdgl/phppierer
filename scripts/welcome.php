0ae9812ba6ce4e25d36659203382026271e90fae
Dean Smith <mrdeansmith01@gmail.com>
2024-08-13 19:35:18 +0100

    Description of the changes

---
+++ scripts/welcome.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "===================================\n";
+echo "  Welcome to the PHPPierer Framework\n";
+echo "===================================\n";
+echo "Thank you for installing PHPPierer!\n";
+echo "Here are some commands to get you started:\n\n";
+echo "  - To start the development server: php servant start\n";
+echo "  - To run migrations: php servant migrate\n";
+echo "  - For help: php servant help\n\n";
+echo "For full documentation, visit: https://github.com/mrmdgl/phppierer\n";
